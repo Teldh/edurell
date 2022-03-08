0fd78e8bb524aaa98fff977489c1ae81e6df8eed
Riccardo Poli <S4522674@studenti.unige.it>
2022-03-08 17:04:32 +0100

    Fixed Popup

--- EVA apps/EdurellVideoAugmentation/src/react-app/src/components/Graph/Popup/Popup.js
+++ EVA apps/EdurellVideoAugmentation/src/react-app/src/components/Graph/Popup/Popup.js
@@ -9,21 +9,22 @@ export default class Popup extends React.Component{
 
     channel = new BroadcastChannel('react-connect'); //create a boradcast channel
 
-    cList = this.props.conceptList === undefined ? "" : this.props.conceptList
-    conceptListElements = cList.forEach((index, value) => {
-        <>
-            <text>
-                Concept : {value} 
-            </text>
-            <br/><button onClick={()=>this.props.goToTimestamp(this.props.conceptTimeBeginList[index])} > GoToConcept : {this.props.conceptTimeBeginList[index]}</button>
-            <br/><button onClick={()=>this.channel.postMessage({to: 'App', msg: this.props.conceptList[index]})} > Open detailed description </button>
-        </>
-    })
-
     render(){
         return(this.props.visible &&
             <div className="popup" style={{left: `${this.props.x}px`, top: `${this.props.y}px`, display: 'inline-block'}}>
-                <p>dsdsd</p>
+                { this.props.conceptList && this.props.conceptList.map((value, id) => 
+                <ul className="ul-popup">
+                    <li className="popup-li">
+                        Concept: <span className="text-concept-popup">{value}</span> 
+                    </li>
+                    <div className="row">
+                        <button className="popup-button" onClick={()=>this.props.goToTimestamp(this.props.conceptTimeBeginList[id])} > GoToConcept : {this.props.conceptTimeBeginList[id]}</button>
+                    </div>
+                    <div className="row">
+                        <button className="popup-button" onClick={()=>this.channel.postMessage({to: 'App', msg: this.props.conceptList[id]})} > Open detailed description </button>
+                    </div>
+                </ul>) 
+                }
             </div>
         )
     }
