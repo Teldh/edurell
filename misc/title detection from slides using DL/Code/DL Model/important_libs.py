13dbc30fca4d0eb359846dcd5661634b04f6260f
SyedMuhammadMujtabaZaidi <127404088+SyedMuhammadMujtabaZaidi@users.noreply.github.com>
2023-11-24 09:35:36 +0100

    important_libs.py

--- misc/title detection from slides using DL/Code/DL Model/important_libs.py
+++ misc/title detection from slides using DL/Code/DL Model/important_libs.py
@@ -1,1 +1,32 @@
+# These two lines are used in Google Colab to mount Google Drive.
+from google.colab import drive
+drive.mount('/content/drive')
 
+# This line imports the OpenCV library, which is used for computer vision tasks, including image and video processing.
+import cv2
+# This line imports the NumPy library, which is used for numerical and mathematical operations in Python. NumPy is commonly used for array manipulation and data handling.
+
+import numpy as np
+# This line imports the 'os' module, which provides a way to interact with the operating system, including functions for working with files and directories.
+
+import os
+# This line imports the TensorFlow library, a popular deep learning framework used for building and training neural networks.
+
+import tensorflow as tf
+# This line imports the train_test_split function from the scikit-learn library, which is commonly used for splitting datasets into training and testing sets.
+from sklearn.model_selection import train_test_split
+# This line imports the Flatten layer from the Keras library. Keras is an API for building and training deep learning models, 
+# and the Flatten layer is used to convert multi-dimensional input into a one-dimensional vector.
+from keras.layers import Flatten
+# These lines import specific layers from TensorFlow's Keras API. LSTM is a Long Short-Term Memory layer, commonly used in recurrent neural networks. 
+# Dense is a fully connected layer, and Dropout is used for regularization to prevent overfitting in neural networks.
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+# This line imports the L2 regularization method from TensorFlow. L2 regularization is used to add a penalty term to the loss function to prevent overfitting.
+from tensorflow.keras.regularizers import l2
+# This line imports the EarlyStopping callback from TensorFlow. 
+# It's used during model training to stop training early if certain conditions are met, typically to prevent overfitting.
+from tensorflow.keras.callbacks import EarlyStopping
+
+# This line imports the Matplotlib library, which is used for creating visualizations and plotting data. 
+# In this case, it's imported as plt.
+import matplotlib.pyplot as plt
